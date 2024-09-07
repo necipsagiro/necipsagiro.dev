5f357d18ff3cb70624e3d426628fda112b83171e
Necip Baha Sağıroğlu <sagiroglun22@itu.edu.tr>
2024-09-07 23:15:07 +0300

    add favicon

--- controllers/boun-senlikci/get.js
+++ controllers/boun-senlikci/get.js
@@ -1,7 +1,7 @@
 export default (req, res) => {
   return res.render('boun-senlikci/index', {
     page: 'boun-senlikci/index',
-    title: 'Boun Şenlikçi',
+    title: 'boun şenlikçi',
     includes: {
       css: ['page', 'general', 'form'],
       js: ['page', 'form']
